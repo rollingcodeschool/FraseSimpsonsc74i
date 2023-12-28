47aae33820a08bbba4188f2bf01fdc2d821ed87b
Emilse Arias <emilsearias@gmail.com>
2023-12-28 10:13:13 -0300

    agregamos el maquetado enviado por sebastian

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { Button, Container } from 'react-bootstrap';
+import logo from './assets/logosimpson.png';
+import Frase from './components/Frase';
+
 
 function App() {
  
   return (
     <>
-      <h1>Frases simpsons</h1>
+    <Container className='my-4 text-center'>
+      <img src={logo} alt="Logo de los simpsons" className='img-fluid'/>
+      <Frase></Frase>
+      <Button className='btnFrase'>Obtener frase</Button>
+    </Container>
     </>
   )
 }
---
+++ src/components/Frase.jsx
@@ -0,0 +1,16 @@
+const Frase = () => {
+    return (
+        <div className="card my-4">
+            <div className="card-body text-center">
+                <h2>Nelson Muntz</h2>
+                <hr />
+                <img src="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png" alt="personaje simpsons" className="img-fluid"/>
+                <div className="my-4 p-4 frase">
+                    <p>Shoplifting is a victimless crime, like punching someone in the dark.</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Frase;
